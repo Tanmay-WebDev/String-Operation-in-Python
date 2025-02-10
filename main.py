9fc10907a5f0774f7cf14a722572bb302edcc7ed
DeveloperTanmay <umas81261@gmail.com>
2025-02-10 19:23:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# String Operation
+
+firstName = "Tanmay"
+lastName = "Singh"
+
+fullName = firstName+lastName
+
+stringInt = "Hello  "*5
+
+print("First Name :", firstName)
+print("Last Name :", lastName)
+print("Full Name :", fullName)
+print("String Multipy with integer :", stringInt)
+
+word = "Programming"
+print("Length of the Word :", len(word))
+print("First Letter of Word :", word[0])
+print("Last Letter of Word :", word[10])
+print("String Sliced :", word[0:4])
+
